f297714c942180a0070c9a76c191b7b823a84e61
Israel-Miles <izzyjmiles@gmail.com>
2020-08-19 13:36:50 -0600

    add homepage and link activities with intents

--- app/src/main/java/com/challenge/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/challenge/passwordmanager/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.challenge.passwordmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+            btnRegister.setOnClickListener {
+                val intent = Intent(this@MainActivity, CreateAccountActivity::class.java)
+                startActivity(intent)
+            }
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
