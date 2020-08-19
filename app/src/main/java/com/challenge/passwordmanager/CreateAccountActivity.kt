baadbe8916e0652738aa9012a5f05d0d9c9ca59c
Israel-Miles <izzyjmiles@gmail.com>
2020-08-19 10:35:13 -0600

    add initialise functions and variable declarations

---
+++ app/src/main/java/com/challenge/passwordmanager/CreateAccountActivity.kt
@@ -0,0 +1,54 @@
+package com.challenge.passwordmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    private val TAG = "CreateAccountActivity"
+
+    //global variables
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var email: String? = null
+    private var password: String? = null
+
+    // UI elements
+    private var etFirstName: EditText? = null
+    private var etLastName: EditText? = null
+    private var etEmail: EditText? = null
+    private var etPassword: EditText? = null
+    private var btnCreateAccount: Button? = null
+
+    // Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        initialise()
+    }
+
+    private fun initialise() {
+        etFirstName = findViewById<View>(R.id.et_first_name) as EditText
+        etLastName = findViewById<View>(R.id.et_last_name) as EditText
+        etEmail = findViewById<View>(R.id.et_email) as EditText
+        etPassword = findViewById<View>(R.id.et_password) as EditText
+        btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        btnCreateAccount!!.setOnClickListener { createNewAccount() }
+    }
+}
