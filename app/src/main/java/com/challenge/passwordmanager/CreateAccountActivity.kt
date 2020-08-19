b772ef7559e8a9411eef5ccff29e0e87a1ace421
Israel-Miles <izzyjmiles@gmail.com>
2020-08-19 11:04:21 -0600

    implement createNewAccount method to store user credentials

--- app/src/main/java/com/challenge/passwordmanager/CreateAccountActivity.kt
+++ app/src/main/java/com/challenge/passwordmanager/CreateAccountActivity.kt
@@ -2,9 +2,13 @@ package com.challenge.passwordmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -25,6 +29,7 @@ class CreateAccountActivity : AppCompatActivity() {
     private var etEmail: EditText? = null
     private var etPassword: EditText? = null
     private var btnCreateAccount: Button? = null
+    private var mProgressBar: ProgressBar? = null
 
     // Firebase references
     private var mDatabaseReference: DatabaseReference? = null
@@ -44,11 +49,55 @@ class CreateAccountActivity : AppCompatActivity() {
         etEmail = findViewById<View>(R.id.et_email) as EditText
         etPassword = findViewById<View>(R.id.et_password) as EditText
         btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
+        mProgressBar = ProgressBar(this)
 
         mDatabase = FirebaseDatabase.getInstance()
         mDatabaseReference = mDatabase!!.reference.child("Users")
         mAuth = FirebaseAuth.getInstance()
 
         btnCreateAccount!!.setOnClickListener { createNewAccount() }
+    }
+
+    private fun createNewAccount() {
+        firstName = etFirstName?.text.toString()
+        lastName = etLastName?.text.toString()
+        email = etEmail?.text.toString()
+        password = etPassword?.text.toString()
+
+        // if user has entered input for every field
+        if (!TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName)
+            && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+
+            mAuth!!
+                .createUserWithEmailAndPassword(email!!, password!!)
+                .addOnCompleteListener(this) { task ->
+                    mProgressBar!!.setVisibility(View.INVISIBLE)
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+
+                        val userId = mAuth!!.currentUser!!.uid
+
+                        //Verify Email
+//                        verifyEmail()
+
+                        //update user profile information
+                        val currentUserDb = mDatabaseReference!!.child(userId)
+                        currentUserDb.child("firstName").setValue(firstName)
+                        currentUserDb.child("lastName").setValue(lastName)
+                        currentUserDb.child("email").setValue(email)
+                        currentUserDb.child("password").setValue(password)
+
+//                        updateUserInfoAndUI()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this@CreateAccountActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
+        }
     }
 }
